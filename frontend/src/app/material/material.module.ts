5481251bbc6fd052f411d34be781751083bebb6c
sethcharris <schristianharris@gmail.com>
2019-12-23 12:38:22 -0600

    Frontend changes. Voting frontend in place.

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -2,8 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
-const MaterialComponents = [MatIconModule, MatCardModule, MatButtonModule];
+const MaterialComponents = [
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatTabsModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSelectModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
